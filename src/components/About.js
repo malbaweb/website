1ac342392a43da56d37e4d91f793b51dcd09e55d
malbaweb <167823509+malbaweb@users.noreply.github.com>
2024-04-22 18:47:36 -0400

    change text in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,11 @@ function About() {
     <div id="about" className="about">
       <h2>Acerca de mi </h2>
       <p>
-        Hola y en esta oportunidad estamaremos viendo como hacer nuestro sitio con react + github actions + github pages.
+        Hola y en esta oportunidad estaremos viendo como hacer nuestro sitio con react + github actions + github pages.
       </p>
+     
       <p>
-        Soy Organizador de la comunidad de Elastic Search Chile y SysArmy Chile, donde me encargo de organizar eventos y charlas para la comunidad.
-        De profesion soy Ingeniero Civil en Informatica y Telecomunicaciones de la Universidad Diego portales, tengo postitulo en ingeligencia artificilia y en
-        ingenieria de software de la Universidad de Chile. Actulamente me desempeño como Head Architect & Software Developer en la Universidad de Chile.
-      </p>
-      <p>
-        Estoy disponible para conversar y debatir distintos puntos de vistas.
+       Feliz viernes :D
       </p>
     </div>
   );
